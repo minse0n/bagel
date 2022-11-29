29e7af0e7b6c170326279b03bb1b72bdc312887b
Hyun Kim <kmasterh@gmail.com>
2022-11-29 23:25:01 +0100

    Create Model Class User,Card

---
+++ frontend/src/app/card.model.ts
@@ -0,0 +1,12 @@
+export class Card {
+  constructor(
+    cardId: string,  // card id
+    userName: string,  // user nickname
+    pictureUrl: string,  // URL for user profile photo (optional)  
+    category: string,  // category of card
+    semester: string,  // semester (type: Semester)
+    course: string,  // name of course (type: Course)
+    title: string,  // title of card 
+    content: string,  // content of card
+  ){}
+}
