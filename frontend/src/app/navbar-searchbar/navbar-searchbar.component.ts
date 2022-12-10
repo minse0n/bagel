a5e70b85e6fac1380a23dcd7567b1f2697a1e873
minseon.kim <minseon.kim@rwth-aachen.de>
2022-12-10 14:41:20 +0100

    Create Component Nav-Search Bar

---
+++ frontend/src/app/navbar-searchbar/navbar-searchbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-searchbar',
+  templateUrl: './navbar-searchbar.component.html',
+  styleUrls: ['./navbar-searchbar.component.scss']
+})
+export class NavbarSearchbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
