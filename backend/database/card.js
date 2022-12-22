337bd70d572e184a7629c24e8d342875af59ae12
10Ho9 <48605359+10Ho9@users.noreply.github.com>
2022-12-22 16:56:18 +0100

    Backend (#58)
    * Update model(card, categories)

--- backend/database/card.js
+++ backend/database/card.js
@@ -2,18 +2,45 @@ import Mongoose from 'mongoose';
 
 const cardSchema = new Mongoose.Schema(
   {
-    id: { type: String, required: true },
     title: { type: String, required: true },
     text: { type: String, requierd: true },
     category: { type: String, requierd: true },
     username: { type: String, requierd: true },
+    avataUrl: { type: String },
     term: { type: String, requierd: true },
     course: { type: String, requierd: true },
-  }
+  }, { timestamps: true, versionKey: false }
 );
 
-const Card = Mongoose.model('Card', cardSchema);
+const Card = Mongoose.model('cards', cardSchema);
 
-export async function getAll() {
-  return Card.find().sort({ creat})
+export async function getAll(){
+  return Card.find({});
+}
+
+export async function getList(){
+  return Card.find({}, { id: 1, title: 1, category: 1, username: 1, term: 1, course: 1 });
+}
+
+export async function getCard(id){
+  return Card.findById(id);
+}
+
+export async function create(title, text, category, term, course){
+  return new Card({
+    title,
+    text,
+    username: 'yoseob',
+    category,
+    term,
+    course
+  }).save();
+}
+
+export async function update(id, title, text, category, term, course) {
+  return Card.findByIdAndUpdate(id, { title, text, category, term, course }, { returnOriginal: false });
+}
+
+export async function remove(id) {
+  return Card.findByIdAndDelete(id);
 }
