d6da21175f1391cc16b08e52b88bbc38d43f17a6
Yoseob Shim <59021311+FelixDev0518@users.noreply.github.com>
2022-12-22 17:42:52 +0100

    Create Docker-compose.yml #16 (#60)
    - Fix error with docker-compose.yml
    
    Co-authored-by: root <root@DESKTOP-IC95H3R.localdomain>

--- backend/database/database.js
+++ backend/database/database.js
@@ -3,10 +3,15 @@ import { config } from '../config.js';
 
 export async function connectDB() {
   //Local test background
-  mongoose.connect(
-    'mongodb://admin:password@mongodb:27017/', { useUnifiedTopology: true })
+  await Mongoose.connect(
+    'mongodb://mongodb:27017/bagel_db', {
+      authSource: "admin",
+      user: "admin",
+      pass: "password",
+      useNewUrlParser: true,
+      useUnifiedTopology: true})
      .then(() => console.log('MongoDB Connected'))
      .catch(err => console.log(err));
   //Main DB test background
-  //return Mongoose.connect(config.db.host);
+  // return Mongoose.connect(config.db.host);
 }
