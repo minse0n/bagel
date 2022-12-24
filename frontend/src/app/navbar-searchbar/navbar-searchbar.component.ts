0bebdb0b2b5c6a078266178fad72ea4f19aa2dca
SunnyJinny <aufkang.hj@gmail.com>
2022-12-24 22:37:11 +0100

    Feature Filter Function for PostCategory#53

--- frontend/src/app/navbar-searchbar/navbar-searchbar.component.ts
+++ frontend/src/app/navbar-searchbar/navbar-searchbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { CATEGORIES } from '../models/post-category';
 
 @Component({
   selector: 'app-navbar-searchbar',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarSearchbarComponent implements OnInit {
 
+  @Output() SelectedCategory = new EventEmitter<string>();
+  
+  categories = CATEGORIES;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+  selectedPostCategory(category: string) {
+    this.SelectedCategory.emit(category);
+  }
 }
