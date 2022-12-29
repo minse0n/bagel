653f0f21b627ca9bed693bd51c4abd68bece3c2b
Hyun Kim <kmasterh@gmail.com>
2022-12-29 13:39:11 +0100

    Search function for Navbar_Searchbar#66 backend

--- backend/database/database.js
+++ backend/database/database.js
@@ -2,11 +2,5 @@ import Mongoose from "mongoose";
 import { config } from '../config.js';
 
 export async function connectDB() {
-  //Local test background
-  mongoose.connect(
-    'mongodb://admin:password@mongodb:27017/', { useUnifiedTopology: true })
-     .then(() => console.log('MongoDB Connected'))
-     .catch(err => console.log(err));
-  //Main DB test background
-  //return Mongoose.connect(config.db.host);
+  return Mongoose.connect(config.db.host);
 }
