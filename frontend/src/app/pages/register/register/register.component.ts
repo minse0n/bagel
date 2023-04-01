4da29a25d2ade2b54736df35c6083f6ec8aa8a34
SunnyJinny <aufkang.hj@gmail.com>
2023-04-02 00:35:52 +0200

    fix:text-editor;refactor:html-tagName

--- frontend/src/app/pages/register/register/register.component.ts
+++ frontend/src/app/pages/register/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CardService } from 'src/app/services/card.service';
+import { BagelCard } from 'src/app/models/bagelCard';
 
 @Component({
   selector: 'app-register',
@@ -9,41 +12,48 @@ import { ActivatedRoute } from '@angular/router';
 export class RegisterComponent implements OnInit {
 
   isEnabled: boolean = false;
-  content: string = '';
-  modules = {};
-    
-  constructor( private route: ActivatedRoute ) { 
-    this.modules = {
-      syntax: true,
-      'emoji-shortname': true,
-      'emoji-textarea': true,
-      'emoji-toolbar': true,
-      'toolbar': [
+  quillConfig = {
+    toolbar: {
+      container: [
         ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
         ['blockquote', 'code-block'],
         [{ 'header': 1 }, { 'header': 2 }],
-      // [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
         [{ 'list': 'ordered' }, { 'list': 'bullet' }],
         [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
-        [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
-        ['link', 'image', 'video'],                         // link and image, video
-        ['emoji']
-      ]
-    }
+      ],
+    },
+  }
+  title: string;
+  text: string;
+  category: string;
+  username: string;
+  term: string;
+  course: string;
+  
+  newCard: BagelCard = {
+    title: '', 
+    text: '',
+    category: '',
+    username: '',
+    term: '', 
+    course: ''
+  }; 
+
+  constructor(
+    private route: ActivatedRoute,
+    private _cardservice: CardService,
+    ) {
   }
   addBindingCreated(quill: { keyboard: { addBinding: (arg0: { key: string; shiftKey?: boolean; }, arg1: { (range: any, context: any): void; (range: any, context: any): void; }) => void; }; }) {
     quill.keyboard.addBinding({
       key: 'b'
     }, (range: any, context: any) => {
-      // tslint:disable-next-line:no-console
       console.log('KEYBINDING B', range, context)
     })
-
     quill.keyboard.addBinding({
       key: 'B',
       shiftKey: true
     }, (range: any, context: any) => {
-      // tslint:disable-next-line:no-console
       console.log('KEYBINDING SHIFT + B', range, context)
     })
   }
@@ -56,5 +66,12 @@ export class RegisterComponent implements OnInit {
     let selected = categorySelect.selectedIndex;
     let selectedValue = categorySelect.options[selected];
     selectedValue.value==='community' ? this.isEnabled = true : this.isEnabled = false;
+  }
+  onFormSubmit() {
+    this._cardservice.create(this.newCard).subscribe({
+      next: (res) => {
+        alert('new Post saved successfully.');
+      }
+    })
   }
 }
