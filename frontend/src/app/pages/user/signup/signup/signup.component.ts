60f9452a700c58fed5a1955db614cebf4c01d846
Hyun Kim <kmasterh@gmail.com>
2023-04-23 09:55:56 +0200

    modify signup page-add pop up msg

--- frontend/src/app/pages/user/signup/signup/signup.component.ts
+++ frontend/src/app/pages/user/signup/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserService } from '../../../../services/user.service'
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-signup',
@@ -10,10 +11,16 @@ import { UserService } from '../../../../services/user.service'
 export class SignupComponent implements OnInit{
   rwthEmail: string;  // email-address from the input
   verificationCode: string;  // verification code received in email
+  googleId: string;  // google id(OAuth)
+  userId: string;  // user id
   sentCode: boolean;  // verification code sent successfully or not 
   verified: boolean;  // Verification of RWTH email successfully or not 
+  googleLoggedin: boolean; // google OAuth logged in or not
 
-  constructor(private userService: UserService) {
+  constructor(
+    private userService: UserService,
+    private toastr: ToastrService
+    ) {
   }
 
   ngOnInit(): void {
@@ -21,6 +28,7 @@ export class SignupComponent implements OnInit{
     this.verificationCode =""; // intiallize verification code 
     this.sentCode = false;
     this.verified = false;
+    this.googleLoggedin = false;
   }
 
   // Verification rwth email
@@ -34,6 +42,11 @@ export class SignupComponent implements OnInit{
       console.log(res);
       if (res.status === 200) {
         this.sentCode = true;
+        this.toastr.info('', 'Verification code has been sent.', {
+          timeOut: 60000,
+          progressBar: true,
+          positionClass: 'toast-top-center',
+        });
       }
     },
     error => {
@@ -55,8 +68,20 @@ export class SignupComponent implements OnInit{
       console.log(res.status);
       console.log(res);
       if (res.status === 200) {
+        this.verified = true;
+        this.googleLoggedin = true;
+        this.toastr.success('', 'Verification success !', {
+          positionClass: 'toast-top-center',
+        });
         console.log('verification success!')
-        window.location.href = 'http://localhost:8080/auth/login/google';
+        // TODO: 구글 로그인 연결 부분 - 추후 수정 필요
+        // window.location.href = 'http://localhost:8080/auth/login/google';
+
+        // 일단 구글 로그인에 성공했다고 가정하고 코드 작성
+        // TODO: 구글 로그인 설정 이후, 수정 필요
+        this.toastr.success('', 'Sign up success !', {
+          positionClass: 'toast-top-center',
+        });
       }
     },
     error => {
