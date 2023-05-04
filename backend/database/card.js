f134942647c82f40aa9588dbfe40a46de8b729c5
10Ho9 <48605359+10Ho9@users.noreply.github.com>
2023-05-04 21:42:08 +0200

    Update card.js

--- backend/database/card.js
+++ backend/database/card.js
@@ -66,10 +66,6 @@ export async function searchCards(keyword) {
   return Card.find({$or: [{ title: keyword }, { text: keyword }]});
 }
 
-export async function searchCards(keyword) {
-  return Card.find({$or: [{ title: keyword }, { text: keyword }]});
-}
-
 export async function update(id, title, text, category, term, course) {
   return Card.findByIdAndUpdate(id, { title, text, category, term, course }, { returnOriginal: false });
 }
