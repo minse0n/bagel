e9ddc7112f7c9a71edb6a79e72aa8f254bc47786
SunnyJinny <hyejin.kang@rwth-aachen.de>
2023-05-06 17:15:54 +0200

    fix: rewrite card service

--- frontend/src/app/pages/main/navbar-searchbar/navbar-searchbar.component.ts
+++ frontend/src/app/pages/main/navbar-searchbar/navbar-searchbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, Input, OnInit, Output, SimpleChange, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { CardService } from 'src/app/services/card.service';
 import { CATEGORIES } from '../../../models/post-category';
@@ -10,10 +10,12 @@ import { CATEGORIES } from '../../../models/post-category';
 })
 export class NavbarSearchbarComponent implements OnInit {
   
+  @Input() isCourse: boolean;
   @Input() isSearch: boolean;
   @Output() SelectedCategory = new EventEmitter<string>();
   @Output() InputtedText = new EventEmitter<string>();
-  
+  @ViewChild('searchBox') searchBox!: ElementRef;
+
   categories = CATEGORIES;
   selected: string;
   searchInput: string;
@@ -32,16 +34,18 @@ export class NavbarSearchbarComponent implements OnInit {
 
   searchCard() {
     this.InputtedText.emit(this.searchInput);
-    if(this.searchInput === undefined) {
-      return console.log("Input some keyword...")
+    if (this.searchInput === undefined) {
+      console.log("Input some keyword...");
+    } else if (this.searchInput.trim() === '') {
+      this.InputtedText.emit(this.searchInput);
     } else {
-        if (this.doubbleSubmit) {
-          this.doubbleSubmit = false;
-          return
-        } else {
-          this.doubbleSubmit = true;
-          return this.cardservice.searchCard(this.searchInput);
-       }
+      this.InputtedText.emit(this.searchInput);
+      if (this.doubbleSubmit) {
+        this.doubbleSubmit = false;
+        return
+      } else {
+        this.doubbleSubmit = true;
+      }
     }
   }
 }
--- frontend/src/app/services/card.service.ts
+++ frontend/src/app/services/card.service.ts
@@ -34,31 +34,58 @@ export class CardService {
     return this._http.delete(`${this.cardUrl}/${id}`);
   }
   
-  searchCard (search: string): Observable<BagelCard[]> {
-    console.log(search);
-    return this._http.get<BagelCard[]>(this.cardsUrl, {
-      params: new HttpParams().set('search', search)
-    });
+  // new function for card filter
+  filterCards(filter: any[]): Observable<BagelCard[]> {
+    return this.getAllData().pipe(map(items => {
+        return this.varFilter(items, filter);
+      })
+    );
+  }
+  varFilter(items: BagelCard[], filter: { filterType: string, filterText: string }[]): any[] {
+    if (filter.length === 0) {
+      return items;
+    }
+    return items.filter(item => {
+      const lastFilter = filter[filter.length - 1];
+      const { filterType, filterText } = lastFilter;
+      if (filterText === 'All') {
+        return items;
+      }
+      if (filterType === 'postCategory') {
+        return item.category && item.category === filterText;
+      }
+      if (filterType === 'searchText') {
+        return item.title && item.title.toLowerCase().includes(filterText.toLowerCase());
+      }
+      if (filterType === 'getCourse') {
+        return item.course && item.course === filterText;
+      }
+      return true;
+    })
   }
   
-  findByCategory(postCategory: string): Observable<BagelCard[]> {
-    console.log(postCategory);
-    return this._http.get<BagelCard[]>(this.cardsUrl, {
-      params: new HttpParams().set('category', postCategory)
-    });
-  }
-
-  findBySearchCategory(searchText: string, postCategory: string): Observable<BagelCard[]> {
-    return this.searchCard(searchText).pipe(map((items: any[]) => 
-      items.filter((item: { category: string; }) => 
-        item.category === postCategory)))
-  }
-  
-  findByCourse(_course: string): Observable<BagelCard[]> {
-    console.log(_course);
-    return this._http.get<BagelCard[]>(this.cardsUrl, {
-      params: new HttpParams().set('course', _course)
-    });
-  }
-  
+  // 지금은 사용안하지만 추가 어떤 문제가 나올지 모르니까 일단 삭제 보류
+  // findBySearchCategory(searchText: string, postCategory: string): Observable<BagelCard[]> {
+  //   return this.searchCard(searchText).pipe(map((items: any[]) => 
+  //     items.filter((item: { category: string; }) => 
+  //       item.category === postCategory)))
+  // }
+  // searchCard (search: string): Observable<BagelCard[]> {
+  //   // console.log(search);
+  //   return this._http.get<BagelCard[]>(this.cardsUrl, {
+  //     params: new HttpParams().set('search', search)
+  //   });
+  // }
+  // findByCategory(postCategory: string): Observable<BagelCard[]> {
+  //   // console.log(postCategory);
+  //   return this._http.get<BagelCard[]>(this.cardsUrl, {
+  //     params: new HttpParams().set('category', postCategory)
+  //   });
+  // }
+  // findByCourse(_course: string): Observable<BagelCard[]> {
+  //   // console.log(_course);
+  //   return this._http.get<BagelCard[]>(this.cardsUrl, {
+  //     params: new HttpParams().set('course', _course)
+  //   });
+  // } 
 }
